ca8d22046075ab927cd0825e98f719079a3418f5
Pavel <wol1414@gmail.com>
2020-09-12 23:50:05 +0300

    done pay

--- public/js/common.js
+++ public/js/common.js
@@ -203,7 +203,7 @@ function eventHandler() {
 
 	var x = window.location.host;
 	var screenName;
-	screenName = 'main.jpg';
+	screenName = '2-1.jpg';
 
 	if (screenName && x === "localhost:3000") {
 		$(".footer").after("<div class=\"pixel-perfect\" style=\"background-image: url(screen/".concat(screenName, ");\"></div>"));
--- sourse/js/common.js
+++ sourse/js/common.js
@@ -194,7 +194,7 @@ function eventHandler() {
 	// добавляет подложку для pixel perfect
 	var x = window.location.host;
 	let screenName;
-	screenName = 'main.jpg';
+	screenName = '2-1.jpg';
 	if (screenName && x === "localhost:3000") {
 		$(".footer").after(`<div class="pixel-perfect" style="background-image: url(screen/${screenName});"></div>`);
 	}
